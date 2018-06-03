7bac933a79dd5da05c5fad6ee962659979b332cd
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2018-06-03 02:45:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,24 @@ client.on('ready', function() {
 
 
 
-
+ client.on('ready', function(){
+        client.user.setStatus("dnd");
+        var ms = 100000 ;
+        var setGame = [`-help`];
+        var i = -1;
+        var j = 0;
+        setInterval(function (){
+            if( i == -1 ){
+                j = 1;
+            }
+            if( i == (setGame.length)-1 ){
+                j = -1;
+            }
+            i = i+j;
+            client.user.setGame(setGame[i],`http://www.twitch.tv/r3b`);
+        }, ms);100000
+    
+   });
 
 client.on('message', message => {
      if (message.content === "-bot") {
@@ -270,26 +287,6 @@ if(message.content.split(' ')[0] == '-bc') {
         });
     }
 })
-
-
-  client.on('ready', function(){
-        client.user.setStatus("dnd");
-        var ms = 100000 ;
-        var setGame = [`-help`];
-        var i = -1;
-        var j = 0;
-        setInterval(function (){
-            if( i == -1 ){
-                j = 1;
-            }
-            if( i == (setGame.length)-1 ){
-                j = -1;
-            }
-            i = i+j;
-            client.user.setGame(setGame[i],`http://www.twitch.tv/r3b`);
-        }, ms);100000
-    
-   });
 
 
 
