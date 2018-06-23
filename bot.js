e4900bf500456e389de882b257f6fe03dc48ead3
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2018-06-23 06:40:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,6 +122,24 @@ if (command == "tag") {
 
 });
 
+client.on('message', ra3d => {
+var prefix = "!";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'color')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });
+`
+
 client.on('message', message => {
     if (message.content === "!serooms") {
     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
