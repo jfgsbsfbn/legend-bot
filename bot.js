980e5964841276cdd5b5e648275acfc0cdeba41b
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2018-06-23 06:44:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,9 +123,8 @@ if (command == "tag") {
 });
 
 client.on('message', ra3d => {
-var prefix = "!";
                         let args = ra3d.content.split(" ").slice(1).join(" ")
-if(ra3d.content.startsWith(prefix + 'color')) {
+if(ra3d.content.startsWith(prefix + 'cc')) {
     if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
              if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
               ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
