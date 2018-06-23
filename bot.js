47e113a7f9783cf6c21093cd822cb879d0d7bc13
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2018-06-23 12:17:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,21 @@ client.on("channelCreate",  cc => {
 
  
 
+var prefix = "!";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+})
 
 client.on('guildMemberAdd', member => {
     if (!member || !member.id || !member.guild) return;
