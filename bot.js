bfc8f448f0ef4c233fdceec19fcf08b142c019fd
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2018-06-23 09:25:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,26 @@ let welcomer = member.guild.channels.find("name","chat");
       }
       });
 
+client.on('message', message => {
+    if (!message.channel.guild) return;
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("***  ليس معك صلاحيات  ***")
+    var prefix = "!";
+       if (message.content ===   prefix + "add role") {
+    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما طلبت  *** ")
+    message.guild.members.forEach(m => {
+    m.addRole(message.guild.roles.find('name', 'اسم الرتبة'))
+    })
+    }
+    
+       if (message.content ===   prefix + "remove role") {
+    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما طلبت  *** ")
+    message.guild.members.forEach(m => {
+    m.removeRole(message.guild.roles.find('name', 'اسم الرتبة'))
+    })
+    }
+    
+    });
+
 
 client.on('message', message => {
     if (message.content === '!roles') {
